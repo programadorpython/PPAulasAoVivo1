2e8b12545a8b45f47fe510770dd8e2282ab3df47
jefferson <jefferson2004@gmail.com>
2020-06-10 00:37:34 -0400

    Removido textos de if e elif e else

--- missao_title_palavras_intermediarias.py
+++ missao_title_palavras_intermediarias.py
@@ -1,15 +1,17 @@
 def upper_intermediarios(frase):
+    """
+    Junta palavras usando ' ', que foi removido por split() acima
+    Usa um if e else decidir se algo precisa de capitalização usando .title()
+    :param frase:
+    :return: Se estiver na lista nao_upper: use como está, caso contrário, use pal_int.title()
+    """
     nao_upper = ["de", "do", "da", "sou", "é", "são", "e", "", "em", "no", "na", "para", "um", "uma"]
     palavras_string = frase.split()
-
-    # junte coisas usando '', que foi removido por split () acima
-    # use um ternário para decidir se algo precisa de capitalização usando .title ()
-    # se estiver no nao_upper: use como está, caso contrário, use v.title ()
     return ' '.join(pal_int if pal_int in nao_upper else pal_int.title() for pal_int in palavras_string)
 
 
-cidade = "rio de janeiro."
+cidade = "rio de janeiro"
 
-print(cidade)
 print()
-print(upper_intermediarios(cidade))
+print(f"String original acima: \t\t\t\t\t\t\t{cidade}")
+print(f"String modificada, mantendo o 'de' minúsculo: \t{upper_intermediarios(cidade)}")
